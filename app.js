abe9940612262e9bfe6294c5a94aabae52e5e296
luminousmind <193550240+luminousmind@users.noreply.github.com>
2025-01-12 14:36:34 +1100

    feat: boilerplate express application

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const indexRouter = require("./routes/indexRouter");
+const path = require("path");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world!");
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", indexRouter);
 
 const PORT = process.env.PORT || 8080;
 
